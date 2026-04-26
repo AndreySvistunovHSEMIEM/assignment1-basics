1c3c20106bcdbc3311c8764763e7dddc10c9052b
Andrew Svistunov <texnik71rus@gmail.com>
2026-04-26 19:57:58 +0300

    stable cross entropy

--- cs336_basics/layers_utils.py
+++ cs336_basics/layers_utils.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from einops import einsum
 
 from torch import Tensor
-from jaxtyping import Float
+from jaxtyping import Float, Int
 
 
 def silu_activation(x: Float[Tensor, "..."]) -> Float[Tensor, "..."]:
@@ -16,6 +16,7 @@ def softmax(x: Float[Tensor, "..."], dim: int = -1) -> Float[Tensor, "..."]:
     result = exponents / exponents.sum(dim=dim, keepdim=True)
     return result
 
+
 def scaled_dot_product_attention(
         Q: Tensor,
         K: Tensor,
@@ -24,8 +25,19 @@ def scaled_dot_product_attention(
 ) -> Tensor:
     if mask is None:
         mask = torch.tril(
-            torch.ones(Q.shape[-2], K.shape[-1], device=Q.device, dtype=bool)
+            torch.ones(Q.shape[-2], K.shape[-2], device=Q.device, dtype=bool)
         )
     result = einsum(Q, K, "... q d_k, ... k d_k -> ... q k").masked_fill(~mask, float("-inf")) / Q.shape[-1] ** 0.5
+    
     scaled_product = einsum(softmax(result), V, "... q k, ... k d_v -> ... q d_v")
     return scaled_product
+
+
+def cross_entropy(
+        inputs: Float[Tensor, " batch_size vocab_size"],
+        targets: Int[Tensor, " batch_size"]
+) -> Float[Tensor, ""]:
+    selected_logits = torch.gather(inputs, -1, targets.unsqueeze(-1))
+    loss = torch.mean(-selected_logits + torch.logsumexp(inputs, dim=-1, keepdim=True))
+    return loss
+    
